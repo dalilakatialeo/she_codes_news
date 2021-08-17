461a2647ab5968b75b8b7db5c7126a47ff99480d
Dalila K Leo <dalilakatialeo@gmail.com>
2021-08-17 18:42:55 +1000

    form

---
+++ she_codes_news/news/forms.py
@@ -0,0 +1,14 @@
+from django import forms #to use all the super cool django form stuff
+from django.forms import ModelForm
+from .models import NewsStory
+
+
+class StoryForm(ModelForm):
+    class Meta: 
+        #nested class we want django to infer from
+        model = NewsStory
+        #list of fields to be included in the form
+        fields = ['title', 'author', 'pub_date', 'content'] 
+        widgets = {
+            'pub_date'
+        }
