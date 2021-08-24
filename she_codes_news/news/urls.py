d3b966cba2b2d351ef79233b8e4af2c0125c39f7
Dalila K Leo <dalilakatialeo@gmail.com>
2021-08-24 20:41:54 +1000

    24/08

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -5,7 +5,7 @@ app_name = 'news'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>', views.StoryView.as_view(), name= 'story'),
+    path('<int:pk>/', views.StoryView.as_view(), name= 'story'),
     path('add-story/', views.AddStoryView.as_view(),name='newStory'),
     # path('update-story/', views.UpdateStoryView.as_view(), name='updateStory')
 ]
