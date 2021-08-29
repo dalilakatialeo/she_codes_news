4d7da0f0088a43be14171c2a002aa8ea0289b91a
Dalila K Leo <dalilakatialeo@gmail.com>
2021-08-29 19:19:34 +1000

    database migration

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -85,26 +85,26 @@ WSGI_APPLICATION = 'she_codes_news.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'dcu22r6bd69cnu',
-#         'USER': 'xonrbqtxxtfhmo',
-#         'PASSWORD': '510a9e58b53240f98026d9cf9215e0f1ca8ecd804903b2f8fa93ec9827b58efe',
-#         'HOST': 'ec2-54-156-60-12.compute-1.amazonaws.com',
-#         'PORT':  '5432'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dcu22r6bd69cnu',
+        'USER': 'xonrbqtxxtfhmo',
+        'PASSWORD': '510a9e58b53240f98026d9cf9215e0f1ca8ecd804903b2f8fa93ec9827b58efe',
+        'HOST': 'ec2-54-156-60-12.compute-1.amazonaws.com',
+        'PORT':  '5432'
+    }
+}
+
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
